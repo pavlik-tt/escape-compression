368ed4eaa9b45d8b12ed1fdbc2a3fcad5ec21b98
pavlik-tt <120838204+pavlik-tt@users.noreply.github.com>
2024-03-08 00:31:04 +0200

    Create esc-comp-words.py

---
+++ esc-comp-words.py
@@ -0,0 +1,33 @@
+import string as stri
+
+def compress_words(s):
+    p = stri.punctuation
+    result = []
+    already_passed = []
+    repetitions = {}
+    def idk_how_to_name_this(og_str, md_str):
+        if not md_str:
+            return '{}'
+        result = ""
+        skip = 0
+        for i in og_str:
+            if skip:
+                skip -= 1
+                continue
+            if i == md_str[0]:
+                result += '{}'
+                skip = len(md_str)-1
+                continue
+            result += i
+        return result
+    for i,c in enumerate(s.split(' ')):  # i - index; c - character
+        stripped = c.strip(p)
+        if stripped in already_passed:
+            formatted = idk_how_to_name_this(c, stripped).format('\x00')
+            result.append()
+            #           idk if this is gonna work
+            repetitions[i+formatted.find('\x00')] = already_passed.index(stripped)
+            continue
+        result.append(c)
+        already_passed.append(stripped)
+    return " ".join(result)
